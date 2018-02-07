6cf5f7487f4a5e7e17fa5d164cfe5c56bcc7fdff
Nic Jackson <jackson.nic@gmail.com>
2018-02-07 10:39:55 +0000

    Updated to staticaly link

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y git build-essential curl libc6-dev-i386
+sudo apt-get install -y git build-essential curl libc6-armel-cross libc6-dev-armel-cross
 
 curl -L -o ~/${go_version}.tar.gz https://dl.google.com/go/${go_version}.tar.gz 
 tar -C /usr/local -xzf ~/${go_version}.tar.gz
 mkdir /root/go
+
+mkdir /root/gcc_tools
+git clone https://github.com/raspberrypi/tools.git --depth 1 /root/gcc_tools
 
 echo "GOPATH=/root/go" >> /root/.ssh/environment
 echo "PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> /root/.ssh/environment
