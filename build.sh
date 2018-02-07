4a3cdf86c57954b26834ec3f71ba1ae22d95c6de
Nic Jackson <jackson.nic@gmail.com>
2018-02-07 07:21:04 +0000

    Removed goreleaser from releasing to the wrong repo

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ git clone https://github.com/nicholasjackson/nomad-arm6l.git
 cp ./nomad-arm6l/.goreleaser.yml $GOPATH/src/github.com/hashicorp/nomad
 
 cd $GOPATH/src/github.com/hashicorp/nomad
-goreleaser --skip-validate --rm-dist
+goreleaser --skip-validate --rm-dist --skip-publish  
